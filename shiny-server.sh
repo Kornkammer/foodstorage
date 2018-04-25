2ce1986201a32953ddf3ea2ec48b79d8255194dd
frumentum <frumentum@posteo.de>
2018-04-25 21:53:36 +0200

    kornkammer specific adoptions are done; dockerfile and shiny-server bash script is updated

--- shiny-server.sh
+++ shiny-server.sh
@@ -2,6 +2,7 @@
 
 # Make sure the directory for individual app logs exists
 mkdir -p /var/log/shiny-server
-chown shiny.shiny /var/log/shiny-server
+chown -R shiny:shiny /var/log/shiny-server
+chown -R shiny:shiny /mntData/fsData
 
 exec shiny-server 2>&1
