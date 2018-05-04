3ff5720c31cf30e52230eecca42ef214696e3889
frumentum <frumentum@posteo.de>
2018-05-04 08:53:28 +0200

    dockerfile excluded from package

--- shiny-server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Make sure the directory for individual app logs exists
-mkdir -p /var/log/shiny-server
-chown -R shiny:shiny /var/log/shiny-server
-chown -R shiny:shiny /mntData/fsData
-
-exec shiny-server 2>&1
